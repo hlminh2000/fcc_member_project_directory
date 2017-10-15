ece7a643fe4fb1268bfad89a89f94b2d9656302c
Minh <hlminh2000@gmail.com>
2017-10-15 14:26:17 -0400

    first mutation query to add user

--- graphQlApi/index.js
+++ graphQlApi/index.js
@@ -42,12 +42,48 @@ const UserType = {
     })
 }
 
+const newUserType = {
+  type: GraphQLString,
+  description: 'Adds a new user and returns the user that was added',
+  args: {
+    id:       { type: GraphQLString },
+    username: { type: GraphQLString },
+    email:    { type: GraphQLString },
+  },
+  resolve: (root, args) => dbService()
+    .insert({
+      user_id : args.id,
+      username: args.username,
+      email   : args.email,
+    })
+    .into('users')
+    .then(e => dbService()
+      .select()
+      .from('users')
+      .where({
+        'user_id': args.id
+      })
+      .then(result => {
+        return new Promise(function(resolve, reject) {
+          resolve(JSON.stringify(result[0]))
+        })
+      })
+    )
+}
+
 const ApiRoot = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'ApiRoot',
     description: 'The root graphql query',
     fields: () => ({
       user: UserType
+    })
+  }),
+  mutation: new GraphQLObjectType({
+    name: 'RootMutation',
+    description: 'Mutations to the root level',
+    fields: () => ({
+      newUser: newUserType
     })
   })
 })
